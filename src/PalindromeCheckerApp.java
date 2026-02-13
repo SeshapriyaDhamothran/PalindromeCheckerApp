0b5b308d056b263a21d3ab7a23ebadeed68d99d5
SeshapriyaDhamothran <seshapriyad@gmail.com>
2026-02-13 15:57:48 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,13 @@
+public class PalindromeCheckerApp {
+    /**
+     * *Application Entry point
+     *
+     * This is a java Palindrome Checker App Welcome Message
+     *
+     * @author Developer
+     * @version 1.0
+     */
+    public static void main(String[] args){
+        System.out.println(" Welcome to Palindrome Checker Management System\n Version : 1.0\n System initialized successfully.\n");
+    }
+}
